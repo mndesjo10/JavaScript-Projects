dfca89eee27f9e6c0f2be0c27c91f9e93d3b8b2d
Joel Mendes <mendesjoel07@gmail.com>
2025-08-27 23:16:29 +0100

    Expressions and alert

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,20 @@
+// 1. Declare two number variables
+var Sent1 = "This is the begining of the string";
+var Sent2 = " and this is the end of the string.";
+
+// 2. Concatenate the two strings together
+var fullSentence = Sent1 + Sent2;
+
+// 3. Use window.alert() to display the concatenated string
+window.alert("Alert: " + fullSentence);
+
+// 4. Use document.write() to display the sentece on the page
+document.write("<h2>concatenated String:</h2>");
+document.write(fullSentence);
+
+// 5. Write an expression (mathematical or logical)
+var number1 = 10;
+var number2 = 5;
+var sum = number1 + number2; // Expression: addition
+document.write("<p><br>Epression Result: " + number1 + " + " + number2 + " = " + sum + "</p>");
+
