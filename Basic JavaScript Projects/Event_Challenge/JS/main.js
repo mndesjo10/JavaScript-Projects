d1a7eb02d88680da835c62188352441341bfd735
Joel Mendes <mendesjoel07@gmail.com>
2025-08-28 01:11:15 +0100

    Project functions

---
+++ Basic JavaScript Projects/Event_Challenge/JS/main.js
@@ -0,0 +1,11 @@
+// Function to change button text when hovered
+function changeText() {
+    var button = document.getElementById("myButton");
+    button.textContent = "You're Houvering!";
+}
+
+// Optiona: Reset onmouseover (extra credit)
+function resetText() {
+    var button = document.getElementById("myButton");
+    button.textContent = "Hover Over Me!";
+}
