e3c0599e9a765c12c2670404d26f13c1f0a45f09
Joel Mendes <mendesjoel07@gmail.com>
2025-10-18 14:41:59 +0100

    Project9 Assignment
    Assignment

--- Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -1,15 +1,54 @@
+// Countdown Timer function
 function countdown() {
+    // Get the input from the user
     var seconds = document.getElementById("seconds").value;
+    var timer = document.getElementById("timer");
 
+    // Function to perform the countdown
     function tick() {
         seconds = seconds - 1;
         timer.innerHTML = seconds;
-        var time = setTimeout(tick, 1000);
-        if (seconds == -1) {
+
+        if (seconds > 0) {
+            setTimeout(tick, 1000);
+        } else {
             alert("Time's up!");
-            learTimeout(time);
-            timer.innerHTML = "";
         }
     }
     tick();
+}
+
+// Image Slideshow functions
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    // Clear all active dots
+    for (i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].classList.add("active");
 }
